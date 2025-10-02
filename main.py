227e6f7be5c133b803e255d25a4d2e0f1f313dd3
WaitingAzar <waitingman28@email.com>
2025-10-02 20:15:46 +0300

    test3

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app.include_router(post_task.router)
 app.include_router(get_task.router)
 app.include_router(delete_task.router)
 
-sa
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
